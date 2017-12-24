73c3f1ed7f5efc829ac01a5f3a4675609391922b
Michal Linhart <michal.linhart@mobatime.cz>
2017-12-24 16:16:05 +0100

    * created projects for library and unit testing together with first end-to-end test (not working yet)

---
+++ _src/X2O.Test/EndToEndTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zoka.X2O.Test
+{
+	public class SimpleClass
+	{
+		public int X;
+	}
+
+	[TestClass]
+	public class EndToEndTests
+	{
+		[TestMethod]
+		public void GivenSimplestXml_ItDeserializeCorrectly()
+		{
+			var simple_class = X2OReader.ReadFromFile("ContentData\\EndToEndTests\\SimpleClass.xml") as SimpleClass;
+
+			Assert.IsNotNull(simple_class, "SimpleClass has not desrialized");
+			Assert.AreEqual(5, simple_class.X, "SimpleClass.X should be 5");
+		}
+	}
+}
